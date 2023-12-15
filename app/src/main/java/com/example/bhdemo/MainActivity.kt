71f382d200297bf4d4bfb83e1306874c137c8018
Vahid-Soudagar <mohammadvahidsoudagar264@gmail.com>
2023-12-15 16:42:22 +0530

    permission debugged

--- app/src/main/java/com/example/bhdemo/MainActivity.kt
+++ app/src/main/java/com/example/bhdemo/MainActivity.kt
@@ -1,40 +1,29 @@
 package com.example.bhdemo
 
-import android.Manifest.permission.CAMERA
-import android.Manifest.permission.CAPTURE_AUDIO_OUTPUT
-import android.Manifest.permission.MODIFY_AUDIO_SETTINGS
-import android.Manifest.permission.READ_EXTERNAL_STORAGE
-import android.Manifest.permission.RECORD_AUDIO
-import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-import android.app.Activity
-import android.content.ContentValues
 import android.content.DialogInterface
-import android.content.Intent
-import android.content.IntentSender
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.PackageManagerCompat
 import com.example.bhdemo.databinding.ActivityMainBinding
-import com.example.bhdemo.utils.Utilities
 import com.example.bhdemo.utils.Utilities.Companion.MULTIPLE_PERMISSION_ID
-import com.example.bhdemo.utils.Utilities.Companion.PERMISSION_CAMERA_REQUEST_CODE
 import com.example.bhdemo.utils.Utilities.Companion.appSettingOpen
 import com.example.bhdemo.utils.Utilities.Companion.warningPermissionDialog
-import com.vmadalin.easypermissions.EasyPermissions
-import com.vmadalin.easypermissions.dialogs.SettingsDialog
 
+/**
+ * Author: Vahid Soudagar
+ * MainActivity demonstrating handling multiple permissions
+ */
 class MainActivity : AppCompatActivity() {
 
+    // View binding for the activity
     private lateinit var binding: ActivityMainBinding
 
-    private val multiPermissionList = if(Build.VERSION.SDK_INT >= 33) {
+    // List of permissions based on Android version
+    private val multiPermissionList = if (Build.VERSION.SDK_INT >= 33) {
         arrayListOf(
             android.Manifest.permission.CAMERA,
             android.Manifest.permission.RECORD_AUDIO,
@@ -50,57 +39,76 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Inflate the layout using view binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        // Set click listener for the button
         binding.btnRequest.setOnClickListener {
+            // Check and request multiple permissions
             if (checkMultiplePermission()) {
+                // Perform the operation if all permissions are granted
                 doOperation()
             }
         }
-
     }
 
+    // Method to perform the desired operation
     private fun doOperation() {
         Toast.makeText(this, "All Permissions Granted Successfully!", Toast.LENGTH_LONG).show()
     }
 
+    // Method to check and request multiple permissions
     private fun checkMultiplePermission(): Boolean {
+        // List to store permissions that need to be requested
         val listPermissionNeeded = arrayListOf<String>()
 
+        // Check each permission in the list
         for (permission in multiPermissionList) {
             if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
                 listPermissionNeeded.add(permission)
             }
         }
 
+        // Request permissions if needed
         if (listPermissionNeeded.isNotEmpty()) {
             ActivityCompat.requestPermissions(this, listPermissionNeeded.toTypedArray(), MULTIPLE_PERMISSION_ID)
             return false
         }
 
+        // Return true if all permissions are already granted
         return true
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray, ) {
+    // Override onRequestPermissionsResult to handle permission request results
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        // Handle the result of multiple permissions request
         if (requestCode == MULTIPLE_PERMISSION_ID) {
             if (grantResults.isNotEmpty()) {
                 var isGrant = true
+
+                // Check if all permissions are granted
                 for (element in grantResults) {
                     if (element == PackageManager.PERMISSION_DENIED) {
                         isGrant = false
                     }
                 }
 
+                // Perform the operation if all permissions are granted
                 if (isGrant) {
                     doOperation()
                 } else {
                     var someDenied = false
+
+                    // Check if some permissions were permanently denied
                     for (permission in permissions) {
                         if (!ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
                             if (ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED) {
@@ -109,11 +117,13 @@ class MainActivity : AppCompatActivity() {
                         }
                     }
 
+                    // Show a toast and open app settings if some permissions were permanently denied
                     if (someDenied) {
-                        Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show()
+                        Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show()
                         appSettingOpen(this)
                     } else {
-                        warningPermissionDialog(this){ _:DialogInterface, which:Int ->
+                        // Show a warning dialog and recheck permissions if denied
+                        warningPermissionDialog(this) { _: DialogInterface, which: Int ->
                             when (which) {
                                 DialogInterface.BUTTON_POSITIVE -> checkMultiplePermission()
                             }
--- app/src/main/java/com/example/bhdemo/utils/Utilities.kt
+++ app/src/main/java/com/example/bhdemo/utils/Utilities.kt
@@ -7,21 +7,38 @@ import android.net.Uri
 import android.widget.Toast
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
+/**
+ * Utility class containing common functions for the application.
+ */
 class Utilities {
 
     companion object {
+        // Constant representing the request code for multiple permissions
         const val MULTIPLE_PERMISSION_ID = 14
 
+        /**
+         * Open app settings to allow the user to enable necessary permissions.
+         * @param context The context in which the app settings will be opened.
+         */
         fun appSettingOpen(context: Context) {
-            Toast.makeText(context, "Go to setting and enable permission", Toast.LENGTH_LONG).show()
+            // Show a toast message guiding the user to settings
+            Toast.makeText(context, "Go to settings and enable permission", Toast.LENGTH_LONG).show()
 
+            // Create an intent to open the app settings for the current package
             val settingIntent = Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
             settingIntent.data = Uri.parse("package:${context.packageName}")
+
+            // Start the intent to open app settings
             context.startActivity(settingIntent)
         }
 
-
-        fun warningPermissionDialog(context: Context, listener : DialogInterface.OnClickListener) {
+        /**
+         * Display a warning dialog indicating that all permissions are required for the app.
+         * @param context The context in which the dialog will be displayed.
+         * @param listener The listener to handle the button click in the dialog.
+         */
+        fun warningPermissionDialog(context: Context, listener: DialogInterface.OnClickListener) {
+            // Create a material alert dialog with a warning message
             MaterialAlertDialogBuilder(context)
                 .setMessage("All Permissions are required for this App")
                 .setCancelable(false)
@@ -29,7 +46,5 @@ class Utilities {
                 .create()
                 .show()
         }
-
     }
-
 }
